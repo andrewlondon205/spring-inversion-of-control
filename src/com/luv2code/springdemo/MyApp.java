1887719277125f899a0f14f1b5c6c61a11b824ae
Ramai Alejandria <vyasadev931@gmail.com>
2022-11-07 17:33:18 +0100

    new class and interface implementations

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        //create the object
+        BaseballCoach theCoach = new BaseballCoach();
+
+        //use the object
+        System.out.println(theCoach.getDailyWorkout());
+
+    }
+}
